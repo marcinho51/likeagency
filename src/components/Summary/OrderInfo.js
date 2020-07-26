d0ee0c367355ac5697cc72164e28f079f5e517d9
Marcin <mwandalowski@wp.pl>
2020-07-26 23:09:05 +0200

    Validation done

--- src/components/Summary/OrderInfo.js
+++ src/components/Summary/OrderInfo.js
@@ -5,12 +5,33 @@ import UserData from "./UserData";
 class OrderInfo extends Component {
   state = {
     cashbox: false,
+    confirm1: false,
+    confirm2: false,
+    confirm3: false,
   };
 
-  toCashbox = () => {
+  toDataEntry = () => {
     this.setState({
       cashbox: true,
     });
+  };
+
+  confirmStepOne = () => {
+    this.setState((state) => ({
+      confirm1: !state.confirm1,
+    }));
+  };
+
+  confirmStepTwo = () => {
+    this.setState((state) => ({
+      confirm2: !state.confirm2,
+    }));
+  };
+
+  confirmStepThree = () => {
+    this.setState((state) => ({
+      confirm3: !state.confirm3,
+    }));
   };
 
   render() {
@@ -21,17 +42,55 @@ class OrderInfo extends Component {
         <div class="orderInfo">
           <h1>Podsumowanie zamówienia:</h1>
           <h3>Typ koszulki: {this.props.frontOrBack}</h3>
-          <button>Potwierdź</button>
+          {this.state.confirm1 ? (
+            <button
+              class="confirmButton"
+              onClick={this.confirmStepOne}
+              style={{ backgroundColor: "green" }}
+            >
+              Potwierdzone
+            </button>
+          ) : (
+            <button class="confirmButton" onClick={this.confirmStepOne}>
+              Potwierdź
+            </button>
+          )}
           <button onClick={() => this.props.editStep(1)}>Edytuj</button>
           <h3>Step2</h3>
-          <button>Potwierdź</button>
+          {this.state.confirm2 ? (
+            <button
+              class="confirmButton"
+              onClick={this.confirmStepTwo}
+              style={{ backgroundColor: "green" }}
+            >
+              Potwierdzone
+            </button>
+          ) : (
+            <button class="confirmButton" onClick={this.confirmStepTwo}>
+              Potwierdź
+            </button>
+          )}
           <button onClick={() => this.props.editStep(2)}>Edytuj</button>
           <h3>Step3</h3>
-          <button>Potwierdź</button>
+          {this.state.confirm3 ? (
+            <button
+              class="confirmButton"
+              onClick={this.confirmStepThree}
+              style={{ backgroundColor: "green" }}
+            >
+              Potwierdzone
+            </button>
+          ) : (
+            <button class="confirmButton" onClick={this.confirmStepThree}>
+              Potwierdź
+            </button>
+          )}
           <button onClick={() => this.props.editStep(3)}>Edytuj</button>
-          <button class="toCashbox" onClick={this.toCashbox}>
-            Do kasy
-          </button>
+          {this.state.confirm1 && this.state.confirm2 && this.state.confirm3 && (
+            <button class="toDataEntry" onClick={this.toDataEntry}>
+              Dalej
+            </button>
+          )}
         </div>
       </>
     );
--- src/components/Summary/UserData.js
+++ src/components/Summary/UserData.js
@@ -1,11 +1,192 @@
 import React, { Component } from "react";
 
+import Delivery from "./Delivery";
+
 class UserData extends Component {
-  state = {};
+  state = {
+    name: "",
+    surname: "",
+    street: "",
+    houseNumber: "",
+    flatNumber: "",
+    postcode: "",
+    city: "",
+    phone: "",
+    email: "",
+    allDataEntried: false,
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      [`${event.target.name}`]: event.target.value,
+    });
+  };
+
+  formValidation = () => {
+    if (this.state.name.length < 2) {
+      this.setState({
+        name: "ZA KRÓTKIE IMIĘ",
+      });
+    }
+
+    if (this.state.surname.length < 2) {
+      this.setState({
+        surname: "ZA KRÓTKIE NAZWISKO",
+      });
+    }
+
+    if (this.state.street.length < 2) {
+      this.setState({
+        street: "ZA KRÓTKA NAZWA ULICY",
+      });
+    }
+
+    if (this.state.houseNumber.length < 1) {
+      this.setState({
+        houseNumber: "BŁĘDNY NUMER DOMU",
+      });
+    }
+
+    if (
+      this.state.postcode.length !== 6 &&
+      this.state.postcode.indexOf("-") === -1
+    ) {
+      this.setState({
+        postcode: "BŁĘDNY KOD POCZTOWY",
+      });
+    }
+
+    if (this.state.city.length < 2) {
+      this.setState({
+        city: "BŁĘDNA NAZWA MIASTA",
+      });
+    }
+
+    let isnum = /^\d+$/.test(this.state.phone);
+
+    if (isnum == false || this.state.phone.length !== 9) {
+      this.setState({
+        phone: "BŁĘDNY NUMER TELEFONU",
+      });
+    }
+
+    if (
+      typeof this.state.email.length < 6 ||
+      this.state.email.indexOf("@") === -1
+    ) {
+      this.setState({
+        email: "BŁĘDNY ADRES EMAIL",
+      });
+    }
+
+    if (
+      this.state.name.length >= 2 &&
+      this.state.surname.length >= 2 &&
+      this.state.street.length >= 2 &&
+      this.state.houseNumber.length >= 1 &&
+      this.state.postcode.length === 6 &&
+      this.state.postcode.indexOf("-") !== -1 &&
+      this.state.city.length >= 2 &&
+      isnum == true &&
+      this.state.phone.length === 9 &&
+      this.state.email.length >= 6 &&
+      this.state.email.indexOf("@") !== -1
+    ) {
+      this.setState({
+        allDataEntried: true,
+      });
+    }
+  };
+
   render() {
-    return (
+    return this.state.allDataEntried ? (
+      <Delivery />
+    ) : (
       <>
-        <h1>UserData</h1>
+        <form>
+          <label>
+            Imię:
+            <input
+              type="text"
+              name="name"
+              value={this.state.name}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Nazwisko:
+            <input
+              type="text"
+              name="surname"
+              value={this.state.surname}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Ulica:
+            <input
+              type="text"
+              name="street"
+              value={this.state.street}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Numer domu:
+            <input
+              type="text"
+              name="houseNumber"
+              value={this.state.houseNumber}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Numer mieszkania:
+            <input
+              type="number"
+              name="flatNumber"
+              value={this.state.flatNumber}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Kod pocztowy:
+            <input
+              type="text"
+              name="postcode"
+              value={this.state.postcode}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Miasto:
+            <input
+              type="text"
+              name="city"
+              value={this.state.city}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Telefon:
+            <input
+              type="text"
+              name="phone"
+              value={this.state.phone}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            E-mail:
+            <input
+              type="text"
+              name="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
+          </label>
+        </form>
+        <button onClick={this.formValidation}>Dalej</button>
       </>
     );
   }
