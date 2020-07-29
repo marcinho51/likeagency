2e13ebd2dc4c075334f10f4584eca4203d9a6ecb
Marcin <mwandalowski@wp.pl>
2020-07-29 11:44:02 +0200

    Updates

---
+++ precache-manifest.49b9cc76733502c49ffafc847d9c0f6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3505cc0c104508275ce815130c83346",
+    "url": "/likeagency/index.html"
+  },
+  {
+    "revision": "8a0a2dea20b8fce31ca0",
+    "url": "/likeagency/static/css/main.f699f104.chunk.css"
+  },
+  {
+    "revision": "c99ad2b42f8738d81d82",
+    "url": "/likeagency/static/js/2.4b0a28a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/likeagency/static/js/2.4b0a28a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a0a2dea20b8fce31ca0",
+    "url": "/likeagency/static/js/main.3cfe6597.chunk.js"
+  },
+  {
+    "revision": "55af2d9a1e546d4915a3",
+    "url": "/likeagency/static/js/runtime-main.a8a496c3.js"
+  }
+]);
